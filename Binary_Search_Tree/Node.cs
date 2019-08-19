3496687f2c448b9b004a9f539786fb2edd9a5e9d
Charles Clark <ctcb57@gmail.com>
2019-08-19 14:51:54 -0500

    Initial commit

---
+++ Binary_Search_Tree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Node
+    {
+        int data;
+        Node leftChild;
+        Node rightChild;
+        public Node link;
+
+        public Node (int data)
+        {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+    }
+}
---
+++ Binary_Search_Tree/Search_Tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search_Tree
+{
+    class Search_Tree
+    {
+        Node start;
+        Node tail;
+
+        public void AddToTree(int numberToAdd)
+        {
+
+        }
+
+        public void Insert_At_End( int data)
+        {
+            Node node = new Node(data);
+            if(start == null)
+            {
+                start = null;
+                tail = node;
+            }
+            else
+            {
+                tail.link = node;
+                tail = node;
+            }
+        }
+
+        public void Search(int searchNumber)
+        {
+
+        }
+    }
+}
